5c51279710de252c8b44eb030db6df8751833a46
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-04-01 23:58:58 -0700

    first test

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # wast-forth
 
 WebAssembly AST :point_right: Forth
+
+
+## Usage
+
+### Install
+
+```
+npm i wast-forth
+```
+
+### CLI
+
+```
+genforth test.wast > test.forth
+```
+
+### As a library
+
+```js
+var p = require('wast-praser').parse,
+    g = require('wast-forth').genforth;
+
+console.log(g(p('(module )')));
+// =>
+```
---
+++ test/basic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var p = require('wast-parser').parse,
+    g = require('../lib/').genforth,
+    expect = require('chai').expect;
+
+describe('basic', function () {
+    it('t0', function (done) {
+        var ast = p('(module (func $add (param $a i32) (param $b i32) (i32.add (get_local $a) (get_local $b))))');
+        var frt = g(ast);
+        expect(frt).to.eq(' : add a@ b@ + ; ')
+        done();
+    });
+});

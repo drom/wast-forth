76551059dea2e84369692b636a323da5e4d64282
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-04-02 00:02:59 -0700

    moving to real parameters

--- test/basic.js
+++ test/basic.js
@@ -6,7 +6,7 @@ var p = require('wast-parser').parse,
 
 describe('basic', function () {
     it('t0', function (done) {
-        var ast = p('(module (func $add (param $a i32) (param $b i32) (i32.add (get_local $a) (get_local $b))))');
+        var ast = p('(module (func $add (param $a i32) (param $b i32) (result i32) (i32.add (get_local $a) (get_local $b))))');
         var frt = g(ast);
         expect(frt).to.eq(' : add a@ b@ + ; ')
         done();
